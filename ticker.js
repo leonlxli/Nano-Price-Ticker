bfa4af9916289f266c1e10c7c825eb6681b38e5a
Leon Li <li.lxiao@yahoo.com>
2018-08-21 02:55:36 -0700

    add volume to extension page as well as fix a small bug with % change

--- ticker.js
+++ ticker.js
@@ -5,18 +5,15 @@ websocket = new WebSocket("wss://stream.binance.com:9443/ws/nanobtc@ticker");
 
 websocket.onopen = function(evt) { console.log(evt) };
 websocket.onmessage = function(message) { 
-    price = JSON.parse(message.data)['o'];
+    price = JSON.parse(message.data)['c'];
     percent = parseFloat(JSON.parse(message.data)['P']).toFixed(1);
-    document.getElementById("sat").innerHTML = price + " SATs"
+    document.getElementById("sat").innerHTML = "PRICE: " + price + " SATs"
+    document.getElementById("volume").innerHTML = "VOLUME: " + JSON.parse(message.data)['v'] 
     if(percent>0) {
     	chrome.browserAction.setBadgeBackgroundColor({ color: [124,252,0, 255] });
     } else {
     	chrome.browserAction.setBadgeBackgroundColor({ color: [220,20,60, 255] });
     }
-    percentStr = String(Math.abs(percent).toFixed(1)) + "%";
-    if(Math.abs(percent) < 1 && percent < 0){
-    	percentStr = percentStr.slice(0,1) + percentStr.slice(2)
-    }
-    console.log(percentStr)
+    percentStr = (String(Math.abs(percent).toFixed(1)) + "%").slice(1);
     chrome.browserAction.setBadgeText({ 'text': percentStr });
 };
